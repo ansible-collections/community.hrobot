08514194f9a61a97fb6d1501dbc83085e64f489c
Felix Fontein <felix@fontein.de>
2020-12-06 23:01:41 +0100

    Add boilerplate.

--- plugins/module_utils/failover.py
+++ plugins/module_utils/failover.py
@@ -3,6 +3,10 @@
 # Copyright (c), Felix Fontein <felix@fontein.de>, 2019
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
 import json
 
 from ansible.module_utils.six.moves.urllib.parse import urlencode
