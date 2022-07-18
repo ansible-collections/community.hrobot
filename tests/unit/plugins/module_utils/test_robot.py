0045886378dbb058032defe59bc5a4c2622ba01a
Felix Fontein <felix@fontein.de>
2022-07-18 23:09:15 +0200

    Move licenses to LICENSES/, use SPDX-License-Identifier, mention all licenses in galaxy.yml (#52)
    * Add SPDX license identifiers, mention all licenses in galaxy.yml.
    
    * Add default copyright headers.
    
    * Fix/add more copyright statements.
    
    * Add sanity test.
    
    * Add changelog.
    
    * Remove colon after 'Copyright'.
    
    * Avoid colon after 'Copyright' in lint script.

--- tests/unit/plugins/module_utils/test_robot.py
+++ tests/unit/plugins/module_utils/test_robot.py
@@ -1,6 +1,7 @@
-# Copyright: (c) 2017 Ansible Project
+# Copyright (c) 2017 Ansible Project
 # Copyright (c), Felix Fontein <felix@fontein.de>, 2019-2020
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
