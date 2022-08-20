8079f91752cabb1f1c4b112322c5969b18275cfb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-08-20 20:57:32 +0300

    Add ICMP and TCP established inputs to example firewall rule (#59)
    * Add ICMP and TCP established inputs to example firewall rule
    
    Taken from Hetzner Robot firewall SSH template example.
    
    I've had network issues when doing an `apt update` before this change.
    
    * Update plugins/modules/firewall.py
    
    * Update plugins/modules/firewall.py
    
    * Update plugins/modules/firewall.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/firewall.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/firewall.py
+++ plugins/modules/firewall.py
@@ -154,6 +154,16 @@ EXAMPLES = r'''
     allowlist_hos: yes
     rules:
       input:
+        - name: Allow ICMP protocol, so you can ping your server
+          ip_version: ipv4
+          protocol: icmp
+          action: accept
+        - name: Allow responses to incoming TCP connections
+          ip_version: ipv4
+          protocol: tcp
+          dst_port: '32768-65535'
+          tcp_flags: ack
+          action: accept
         - name: Allow everything to ports 20-23 from 4.3.2.1/24
           ip_version: ipv4
           src_ip: 4.3.2.1/24
