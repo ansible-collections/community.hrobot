fd17ec87cb678bf158e96640dd63f9a1d80c7cd3
Felix Fontein <felix@fontein.de>
2024-12-29 15:47:58 +0100

    Fix doc fragments indents.

--- plugins/doc_fragments/attributes.py
+++ plugins/doc_fragments/attributes.py
@@ -24,24 +24,24 @@ attributes:
     INFO_MODULE = r'''
 options: {}
 attributes:
-    check_mode:
-      support: full
-      details:
-        - This action does not modify state.
-    diff_mode:
-      support: N/A
-      details:
-        - This action does not modify state.
+  check_mode:
+    support: full
+    details:
+      - This action does not modify state.
+  diff_mode:
+    support: N/A
+    details:
+      - This action does not modify state.
 '''
 
     ACTIONGROUP_ROBOT = r'''
 options: {}
 attributes:
-    action_group:
-      description: Use C(group/community.hrobot.robot) in C(module_defaults) to set defaults for this module.
-      support: full
-      membership:
-        - community.hrobot.robot
+  action_group:
+    description: Use C(group/community.hrobot.robot) in C(module_defaults) to set defaults for this module.
+    support: full
+    membership:
+      - community.hrobot.robot
 '''
 
     CONN = r"""
@@ -66,16 +66,16 @@ attributes:
     FACTS_MODULE = r'''
 options: {}
 attributes:
-    check_mode:
-      support: full
-      details:
-        - This action does not modify state.
-    diff_mode:
-      support: N/A
-      details:
-        - This action does not modify state.
-    facts:
-      support: full
+  check_mode:
+    support: full
+    details:
+      - This action does not modify state.
+  diff_mode:
+    support: N/A
+    details:
+      - This action does not modify state.
+  facts:
+    support: full
 '''
 
     FILES = r"""
